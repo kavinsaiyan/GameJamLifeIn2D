a030f1a3d35861ad715d8d1de8fbff5e22e45508
kavinsaiyan <37509336+kavinsaiyan@users.noreply.github.com>
2023-07-03 23:54:58 +0530

    WIP simple button

---
+++ LifeIn2D/Input/SimpleButton.cs
@@ -0,0 +1,16 @@
+using Microsoft.Xna.Framework;
+using MonoGame.Extended;
+using SimplePhysics;
+
+namespace LifeIn2D.Input
+{
+    public class SimpleButton
+    {
+        public AABB rect;
+
+        public SimpleButton(Vector2 pos, Vector2 size)
+        {
+
+        }
+    }
+}
