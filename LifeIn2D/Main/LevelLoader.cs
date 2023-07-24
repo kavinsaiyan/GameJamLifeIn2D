acfd2eaab22c451740da83072f5c3d4b16dbb231
kavinsaiyan <37509336+kavinsaiyan@users.noreply.github.com>
2023-07-24 23:27:55 +0530

    level loader WIP from text files

---
+++ LifeIn2D/Main/LevelLoader.cs
@@ -0,0 +1,15 @@
+using Microsoft.Xna.Framework;
+
+namespace LifeIn2D
+{
+    public class LevelLoader
+    {
+        public const string FILE_PATH = "Text Files/Levels.txt";
+        public int[,] grid;
+        public int currentLevel;
+        public LevelLoader()
+        {
+
+        }
+    }
+}
