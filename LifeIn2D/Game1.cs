466eb388d854e64f895ec0b0d5446f4b1c0d8efc
kavinsaiyan <kavinssj2@gmail.com>
2023-09-21 00:16:29 +0530

    Lv 7

--- LifeIn2D/Game1.cs
+++ LifeIn2D/Game1.cs
@@ -40,7 +40,7 @@ namespace LifeIn2D
             _inputManager = new InputManager();
 
             _levelLoader = new LevelLoader();
-            _levelLoader.currentLevel = 6;
+            _levelLoader.currentLevel = 7;
             _levelLoader.Load();
 
             _gridManager = new GridManager();
