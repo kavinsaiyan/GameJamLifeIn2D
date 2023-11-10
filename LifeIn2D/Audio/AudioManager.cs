df41ebef6265b36343f86e888406e41ed5017951
kavinsaiyan <kavinssj2@gmail.com>
2023-11-10 23:31:03 +0530

    bg music added

--- LifeIn2D/Audio/AudioManager.cs
+++ LifeIn2D/Audio/AudioManager.cs
@@ -7,9 +7,15 @@ namespace LifeIn2D.Audio
     public class AudioManager
     {
         private SoundEffect _clickSoundEffect;
+        private SoundEffect _bgMusicEffect;
+        private SoundEffectInstance _bgSoundEffectInstance;
         public AudioManager(ContentManager content)
         {
             _clickSoundEffect = content.Load<SoundEffect>("Audio/ClickTile");
+            _bgMusicEffect = content.Load<SoundEffect>("Audio/BGTrack");
+            _bgSoundEffectInstance = _bgMusicEffect.CreateInstance();
+            _bgSoundEffectInstance.IsLooped = true;
+            _bgSoundEffectInstance.Play();
         }
 
         public void PlayClickSound()
